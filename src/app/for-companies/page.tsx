1878303e0bd8866367d2ff281b011266fe616770
Egor Ermolaev <egorostu@gmail.com>
2024-11-12 00:25:20 +0100

    fix(address): on mobile browsers

--- src/app/for-companies/page.tsx
+++ src/app/for-companies/page.tsx
@@ -1,40 +1,25 @@
 "use client";
 
-import {defineChain, getContract, prepareContractCall, getAddress} from "thirdweb";
-import { ConnectButton, useSendTransaction} from "thirdweb/react";
-import { useState, useEffect } from "react";
-import { client } from "../client";
+import {defineChain, getContract, prepareContractCall} from "thirdweb";
+import {ConnectButton, useActiveAccount, useSendTransaction,} from "thirdweb/react";
+import {useState, useEffect} from "react";
+import {client} from "../client";
 import GHGEmissionForm from "./GHGEmissionForm";
 import HazardousWasteForm from "./HazardousWasteForm";
 import NonHazardousWasteForm from "./NonHazardousWasteForm";
 import PackagingMaterialForm from "./PackagingMaterialForm";
 import EnergyConsumptionForm from "./EnergyConsumptionForm";
 import WaterConsumptionForm from "./WaterConsumptionForm";
-import { ethers } from "ethers";
+import {ethers} from "ethers";
 import Image from 'next/image';
 import logoIcon from "../../../public/logo.svg";
+
 
 const contract = getContract({
     client,
     chain: defineChain(128123),
     address: "0x33c06A3610D58df2D6c3A8F24063b9b4DEEdB283"
 });
-
-async function getWalletAddress() {
-    // Подключение к провайдеру (MetaMask, например)
-    // @ts-ignore
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-    // Ожидание подключения аккаунта
-    await provider.send("eth_requestAccounts", []);
-
-    // Получение аккаунта
-    const signer = provider.getSigner();
-    const address = await signer.getAddress();
-
-    console.log("Connected wallet address:", address);
-    return address;
-}
 
 export default function ForCompanies() {
     const [address, setAddress] = useState(null);
@@ -57,36 +42,37 @@ export default function ForCompanies() {
     });
     const [submitting, setSubmitting] = useState(false);
 
-    useEffect(() => {
-        async function fetchAddress() {
-            const walletAddress = await getWalletAddress();
-            // @ts-ignore
-            setAddress(walletAddress);
-        }
-
-        // @ts-ignore
-        if (window.ethereum) {
-            // @ts-ignore
-            window.ethereum.on('accountsChanged', async () => {
-                const walletAddress = await getWalletAddress();
-                // @ts-ignore
-                setAddress(walletAddress);
-            });
-            fetchAddress();
-        }
-    }, []);
-
     const categories = [
-        { name: 'GHG Emission', component: GHGEmissionForm, kpis: ['ghgEmissionA11', 'ghgEmissionA12', 'ghgEmissionA15'] },
-        { name: 'Hazardous Waste', component: HazardousWasteForm, kpis: ['hazardousWasteA13', 'hazardousWasteA16'] },
-        { name: 'Non-Hazardous Waste', component: NonHazardousWasteForm, kpis: ['nonHazardousWasteA14', 'nonHazardousWasteA16'] },
-        { name: 'Packaging Material', component: PackagingMaterialForm, kpis: ['packagingMaterialA25', 'environmentalImpactA31', 'climateImpactA41'] },
-        { name: 'Water Consumption', component: WaterConsumptionForm, kpis: ['waterConsumptionA22', 'waterConsumptionA24'] },
-        { name: 'Energy Consumption', component: EnergyConsumptionForm, kpis: ['energyConsumptionA21', 'energyConsumptionA23'] }
+        {
+            name: 'GHG Emission',
+            component: GHGEmissionForm,
+            kpis: ['ghgEmissionA11', 'ghgEmissionA12', 'ghgEmissionA15']
+        },
+        {name: 'Hazardous Waste', component: HazardousWasteForm, kpis: ['hazardousWasteA13', 'hazardousWasteA16']},
+        {
+            name: 'Non-Hazardous Waste',
+            component: NonHazardousWasteForm,
+            kpis: ['nonHazardousWasteA14', 'nonHazardousWasteA16']
+        },
+        {
+            name: 'Packaging Material',
+            component: PackagingMaterialForm,
+            kpis: ['packagingMaterialA25', 'environmentalImpactA31', 'climateImpactA41']
+        },
+        {
+            name: 'Water Consumption',
+            component: WaterConsumptionForm,
+            kpis: ['waterConsumptionA22', 'waterConsumptionA24']
+        },
+        {
+            name: 'Energy Consumption',
+            component: EnergyConsumptionForm,
+            kpis: ['energyConsumptionA21', 'energyConsumptionA23']
+        }
     ];
 
     const CurrentCategoryForm = categories[currentCategoryIndex]?.component;
-    const { mutate: sendTransaction } = useSendTransaction();
+    const {mutate: sendTransaction} = useSendTransaction();
 
     // @ts-ignore
     const stringToUint256 = (str) => {
@@ -137,6 +123,14 @@ export default function ForCompanies() {
         }
     };
 
+    const account = useActiveAccount();
+
+    useEffect(() => {
+        if (account && account.address) { // @ts-ignore
+            setAddress(account.address)
+        }
+    }, [account]);
+
     return (
         <div className="relative p-4 pb-10 min-h-[100vh] container max-w-screen-lg mx-auto">
             <div className="fixed top-4 left-4">
@@ -146,33 +140,35 @@ export default function ForCompanies() {
                         alt="Home"
                         width={50}
                         height={50}
-                        style={{ filter: "drop-shadow(0px 0px 24px #a726a9a8)" }}
+                        style={{filter: "drop-shadow(0px 0px 24px #a726a9a8)"}}
                     />
                 </a>
             </div>
             <div className="fixed top-4 right-4">
-                <ConnectButton client={client} appMetadata={{ name: "FiledFlow - the ESG KPI Reporting System", url: "https://filedflow.lu" }} />
+                <ConnectButton client={client} appMetadata={{ name: "FiledFlow - the ESG KPI Reporting System", url: "https://filedflow.lu" }}/>
             </div>
             <main className="flex items-center justify-center min-h-full">
                 <div className="py-20 text-center">
                     <h1 className="text-3xl md:text-5xl font-bold tracking-tight mb-10 text-zinc-100">
                         For Companies - Submit ESG Data
                     </h1>
-                    <p className="text-zinc-300 text-base mb-6">Submit your ESG data to comply with reporting standards from address: {address}.</p>
+                    <p className="text-zinc-300 text-base mb-6">Submit your ESG data to comply with reporting standards
+                        from address: {address}.</p>
                     <h2 className="text-2xl font-bold mb-4 text-zinc-100">{categories[currentCategoryIndex]?.name}</h2>
                     <div className="flex flex-col items-center gap-4 mt-10">
                         {CurrentCategoryForm && // @ts-ignore
-                             (<CurrentCategoryForm
-                                formData={formData}
-                                setFormData={setFormData}
-                                handleSubmit={handleSubmit}
-                                submitting={submitting}
-                            />
-                        )}
+                            (<CurrentCategoryForm
+                                    formData={formData}
+                                    setFormData={setFormData}
+                                    handleSubmit={handleSubmit}
+                                    submitting={submitting}
+                                />
+                            )}
                         {(!submitting && currentCategoryIndex === categories.length) && (
                             <p className="text-green-500 mt-4">
                                 Thanks for submitting!{' '}
-                                <a href={`/for-investors?company-address=${address}`} className="text-blue-500 underline">
+                                <a href={`/for-investors?company-address=${address}`}
+                                   className="text-blue-500 underline">
                                     Go to investor page
                                 </a>
                             </p>
